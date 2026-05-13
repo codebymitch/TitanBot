cc6f597cb150c89572526ff50cc44b2b6b69abe2
maxsasukeuchiha271-create <maxsasukeuchiha271@gmail.com>
2026-05-13 09:04:33 -0500

    Update mine.js

--- src/commands/Economy/mine.js
+++ src/commands/Economy/mine.js
@@ -6,8 +6,8 @@ import { MessageTemplates } from '../../utils/messageTemplates.js';
 import { InteractionHelper } from '../../utils/interactionHelper.js';
 
 const MINE_COOLDOWN = 60 * 60 * 1000;
-const BASE_MIN_REWARD = 400;
-const BASE_MAX_REWARD = 1200;
+const BASE_MIN_REWARD = 80000;
+const BASE_MAX_REWARD = 80000;
 const PICKAXE_MULTIPLIER = 1.2;
 const DIAMOND_PICKAXE_MULTIPLIER = 2.0;
 
