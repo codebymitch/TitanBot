74fbd7782b8e7184e93a1fa30250311e943891f5
ErednayPsicho <129790973+ErednayPsicho@users.noreply.github.com>
2026-05-17 17:01:41 -0300

    Update bot activity name in bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "Bot favorito do xico-chan",
         // Activity type number (0 = Playing).
         type: 0, 
       },
