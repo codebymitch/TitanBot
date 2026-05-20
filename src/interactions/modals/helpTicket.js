0620c8c25b7dc3120d57e77289903dd01cfa8e7d
ErednayPsicho <pineplestalker64@gmail.com>
2026-05-20 20:07:52 -0300

    support2

---
+++ src/interactions/modals/helpTicket.js
@@ -0,0 +1,11 @@
+/**
+ * Help Ticket Modal Interactions
+ * 
+ * Exports the modal handlers for the support ticket system.
+ */
+
+import { helpCloseModalHandler } from '../../handlers/helpTicketButtons.js';
+
+export default [
+  helpCloseModalHandler,
+];
