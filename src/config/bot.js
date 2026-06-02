5abc3c811e0371a96c1ba33589cf71622d0655c5
EstrogenWarrior <camden.rawlins6@gmail.com>
2026-06-02 17:18:45 +0100

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "Life is an experiment, and we its guinea pigs. You, me, the other Umamusume—everyone!",
         // Activity type number (0 = Playing).
         type: 0, 
       },
