687091d2b70515deec7f6b18afb9f2a9d8997af9
aeddamj-rgb <aeddamj@gmail.com>
2026-06-04 00:25:38 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,9 +25,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "https://discord.com/invite/huY2s5DUbQ❤️",
         // Activity type number (0 = Playing).
-        type: 0, 
+        type: 1, 
       },
     ],
   },
