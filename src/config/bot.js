4b13911779161ed74d72f98320772b39c2bc9fa3
Vyxz1s3 <clymansjerni@gmail.com>
2026-06-17 21:25:25 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,9 +25,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Atlas Design!",
+        name: "Created By Atlas Design!",
         // Activity type number (0 = Playing).
-        type: 4, 
+        type: 3, 
       },
     ],
   },
