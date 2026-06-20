af5d20dfd85b2003666bf8f0c6910df27cc3c0eb
Kjmodz <138188142+Kjmodz@users.noreply.github.com>
2026-06-20 16:29:39 -0600

    Update messageCreate.js

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -47,7 +47,7 @@ export default {
 
 async function handleFAQ(message) {
   const faqs = {
-    "How do I join": "Create a ticket https://discord.com/channels/1382512078585200642/1514857141125644429",
+    "How do I join team syne": "Create a ticket https://discord.com/channels/1382512078585200642/1396618369209340084",
     "how do i make a ticket": "To join team syne click here https://discord.com/channels/1382512078585200642/1514857141125644429",
     "what are the rules": "Please check the rules channel for our server rules!",
     "how do i level up": "Send messages in the server to earn XP and level up!",
