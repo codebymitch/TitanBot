56319e9012d998b9ebfceaf0159812d66d89fd8c
hypercloudbot <hypercloud.bot@myyahoo.com>
2026-08-01 22:26:48 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,7 +23,7 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "Custom Status", // required by Discord API, not shown in the client
+        name: "HyperCloud.bot", // required by Discord API, not shown in the client
         state: "Hypercloud.bot",     // this is what people actually see
         type: 0,               // Custom
       },
